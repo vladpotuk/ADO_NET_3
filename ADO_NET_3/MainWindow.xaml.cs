7b98574dcb46749b9319d7d479d3045f9bb4889e
Потук Владислав Олексійович <143324690+vladpotuk@users.noreply.github.com>
2024-05-12 21:36:47 +0300

    update

--- ADO_NET_3/MainWindow.xaml.cs
+++ ADO_NET_3/MainWindow.xaml.cs
@@ -1,1 +1,224 @@
-﻿
+﻿using Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Windows;
+
+namespace WpfApp_12._05
+{
+    public partial class MainWindow : Window, INotifyPropertyChanged
+    {
+        private string _message;
+        private DBManager _dbManager;
+
+        public string Message
+        {
+            get { return _message; }
+            set
+            {
+                _message = value;
+                OnPropertyChanged("Message");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            _dbManager = new DBManager();
+        }
+
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                _dbManager.OpenConnection();
+                Message = "Connected to the database.";
+            }
+            catch (Exception ex)
+            {
+                Message = "Error connecting to the database: " + ex.Message;
+            }
+        }
+
+        private void DisconnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                _dbManager.CloseConnection();
+                Message = "Disconnected from the database.";
+            }
+            catch (Exception ex)
+            {
+                Message = "Error disconnecting from the database: " + ex.Message;
+            }
+        }
+
+        private void ShowAllStationeryButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                DataTable stationeryTable = _dbManager.GetAllStationery();
+                Message = "Displayed all stationery.";
+            }
+            catch (Exception ex)
+            {
+                Message = "Error displaying stationery: " + ex.Message;
+            }
+        }
+
+        private void ShowAllTypesButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                List<string> types = _dbManager.GetAllStationeryTypes();
+                Message = "Displayed all types of stationery.";
+            }
+            catch (Exception ex)
+            {
+                Message = "Error displaying types of stationery: " + ex.Message;
+            }
+        }
+
+        private void ShowAllSalesManagersButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                List<string> managers = _dbManager.GetAllSalesManagers();
+                Message = "Displayed all sales managers.";
+            }
+            catch (Exception ex)
+            {
+                Message = "Error displaying sales managers: " + ex.Message;
+            }
+        }
+
+        private void ShowMaxUnitsStationeryButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                DataTable maxUnitsStationery = _dbManager.GetStationeryWithMaxUnits();
+                Message = "Displayed stationery with maximum number of units.";
+            }
+            catch (Exception ex)
+            {
+                Message = "Error displaying stationery with maximum number of units: " + ex.Message;
+            }
+        }
+
+        private void ShowMinUnitsStationeryButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                DataTable minUnitsStationery = _dbManager.GetStationeryWithMinUnits();
+                Message = "Displayed stationery with minimum number of units.";
+            }
+            catch (Exception ex)
+            {
+                Message = "Error displaying stationery with minimum number of units: " + ex.Message;
+            }
+        }
+
+        private void ShowMinCostStationeryButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                DataTable minCostStationery = _dbManager.GetStationeryWithMinCost();
+                Message = "Displayed stationery with minimum unit cost.";
+            }
+            catch (Exception ex)
+            {
+                Message = "Error displaying stationery with minimum unit cost: " + ex.Message;
+            }
+        }
+
+        private void ShowMaxCostStationeryButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                DataTable maxCostStationery = _dbManager.GetStationeryWithMaxCost();
+                Message = "Displayed stationery with maximum unit cost.";
+            }
+            catch (Exception ex)
+            {
+                Message = "Error displaying stationery with maximum unit cost: " + ex.Message;
+            }
+        }
+
+        
+        private void ShowStationeryByTypeButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string selectedType = "";
+                DataTable stationeryByType = _dbManager.GetStationeryByType(selectedType);
+                Message = $"Displayed stationery of type: {selectedType}.";
+            }
+            catch (Exception ex)
+            {
+                Message = "Error displaying stationery by type: " + ex.Message;
+            }
+        }
+
+        private void ShowStationeryByManagerButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string selectedManager = "";
+                DataTable stationeryByManager = _dbManager.GetStationeryByManager(selectedManager);
+                Message = $"Displayed stationery sold by manager: {selectedManager}.";
+            }
+            catch (Exception ex)
+            {
+                Message = "Error displaying stationery by manager: " + ex.Message;
+            }
+        }
+
+        private void ShowStationeryByBuyerButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                string selectedBuyer = "";
+                DataTable stationeryByBuyer = _dbManager.GetStationeryByBuyer(selectedBuyer);
+                Message = $"Displayed stationery purchased by buyer: {selectedBuyer}.";
+            }
+            catch (Exception ex)
+            {
+                Message = "Error displaying stationery by buyer: " + ex.Message;
+            }
+        }
+
+        private void ShowRecentSaleButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                DataRow recentSale = _dbManager.GetRecentSale();
+                Message = "Displayed information about recent sale.";
+            }
+            catch (Exception ex)
+            {
+                Message = "Error displaying information about recent sale: " + ex.Message;
+            }
+        }
+
+        private void ShowAverageItemsPerTypeButton_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Dictionary<string, double> averageItemsPerType = _dbManager.GetAverageItemsPerType();
+                Message = "Displayed average number of items for each type of stationery.";
+            }
+            catch (Exception ex)
+            {
+                Message = "Error displaying average items per type: " + ex.Message;
+            }
+        }
+    }
+}
--- DataBase.cs/DataBase.cs
+++ DataBase.cs/DataBase.cs
@@ -1,1 +1,199 @@
-﻿
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data
+{
+    public class Test
+    {
+        public int Id { get; set; }
+        public string Text { get; set; }
+
+        public Test(int id, string text)
+        {
+            Id = id;
+            Text = text;
+        }
+
+        public override string ToString()
+        {
+            return $"Id: {Id}, Text: {Text}";
+        }
+    }
+
+    public class DBManager
+    {
+        public string ConnectionString = "Data Source=10.0.0.50,1413;Initial Catalog=ado_test;User ID=strof;Password=1604;Encrypt=True;Trust Server Certificate=True";
+
+        public DBManager()
+        {
+
+        }
+
+        public List<Test> SelectFromDb()
+        {
+            try
+            {
+                List<Test> result = new List<Test>();
+                using (SqlConnection connection = new SqlConnection(this.ConnectionString))
+                {
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand("SELECT * FROM test", connection);
+                    var sqlReader = cmd.ExecuteReader();
+                    while (sqlReader.Read())
+                    {
+                        result.Add(new Test(sqlReader.GetInt32(0), sqlReader.GetString(1)));
+                    }
+                }
+                return result;
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void InsertToDb(Test test)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(this.ConnectionString))
+                {
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand("INSERT INTO test (text) VALUES (@text)", connection);
+                    cmd.Parameters.AddWithValue("@text", test.Text);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void UpdateToDb(Test test)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(this.ConnectionString))
+                {
+                    connection.Open();
+                    SqlCommand cmd = new SqlCommand("UPDATE test SET text = @text WHERE id = @id", connection);
+                    cmd.Parameters.AddWithValue("@text", test.Text);
+                    cmd.Parameters.AddWithValue("@id", test.Id);
+                    cmd.ExecuteNonQuery();
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void OpenConnection()
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(this.ConnectionString))
+                {
+                    connection.Open();
+                   
+                }
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public void CloseConnection()
+        {
+            try
+            {
+ 
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public DataTable GetAllStationery()
+        {
+            try
+            {
+                DataTable result = new DataTable();
+                using (SqlConnection connection = new SqlConnection(this.ConnectionString))
+                {
+                    connection.Open();
+                    SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM Stationery", connection);
+                    adapter.Fill(result);
+                }
+                return result;
+            }
+            catch (SqlException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public List<string> GetAllStationeryTypes()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<string> GetAllSalesManagers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetStationeryWithMaxUnits()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetStationeryWithMinUnits()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetStationeryWithMinCost()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetStationeryWithMaxCost()
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetStationeryByType(string selectedType)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetStationeryByManager(string selectedManager)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataTable GetStationeryByBuyer(string selectedBuyer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public DataRow GetRecentSale()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dictionary<string, double> GetAverageItemsPerType()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
